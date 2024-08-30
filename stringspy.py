92bccc9f6831a6ed3898f4fc1ac8305ebbdb4f66
KENGUVA HARIKRISHNA <121809229+HariKrishna138@users.noreply.github.com>
2024-08-30 12:19:36 +0530

    Add files via upload

---
+++ stringspy.py
@@ -0,0 +1,27 @@
+t="hello vitb"
+t1="world"
+t3="VITB"
+t4="bhimavaram534202"
+r="532460"
+print(t+t1)
+print(t*4)
+for i in t:
+    print(i)
+print('o' in t and 'o' in t1)
+print(t[1:3])
+print(t[::-1])
+print(t[-1:-4:-1])
+print(t.capitalize())
+print(t1.upper())
+print(t.isupper())
+print(t1.islower())
+print(t.isalpha())
+print(t3.lower())
+print(t.title())
+print(t1.replace('w', 'W'))
+print(t3.find('T'))
+print("{cname} BHIMAVARAM".format(cname = "VITB"))
+print(t4.isalnum())
+print(r.isdigit())
+
+
